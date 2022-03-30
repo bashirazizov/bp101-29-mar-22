1c5e1fbe8d29f889fe284945ded527067724da2e
Bashir Azizov <89950169+bashirazizov@users.noreply.github.com>
2022-03-30 14:57:48 +0400

    Add files via upload

---
+++ ClassLibrary1/ClassLibrary1/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Notification
+    {
+        public int Name { get; set; }
+        protected internal void Notify(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+        }
+
+        protected internal string Surname { get; set; }
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Test
+    {
+        public void test()
+        {
+            Notification n = new Notification();
+            n.Notify("Necesen", ConsoleColor.Green);
+
+        }
+    }
+}
